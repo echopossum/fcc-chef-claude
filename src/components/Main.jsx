a212361c3d023f4b7d60b1f410e0e579d11c925b
Echo <echopossum@gmail.com>
2025-01-04 00:14:35 -0500

    Start ingredient form

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <main>
+      <form className="ingredientForm">
+        <input className="ingredientInput" type="text" />
+        <button className="addButton">+ Add Ingredient</button>
+      </form>
+    </main>
+  );
+};
+
+export default Main;
