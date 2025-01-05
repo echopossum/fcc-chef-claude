00751566c60962a614911326bb54732a20f5bdf8
Echo <echopossum@gmail.com>
2025-01-05 14:19:30 -0500

    Add input state functionallity

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 const Main = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  const ingredientsList = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("form subbmitted");
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  };
+
   return (
     <main>
-      <form className="ingredientForm">
+      <form className="ingredientForm" onSubmit={handleSubmit}>
         <input
           className="ingredientInput"
           type="text"
           aria-label="Add Ingredient"
           placeholder="e.g. oregano"
+          name="ingredient"
         />
         <button className="addButton">+ Add Ingredient</button>
       </form>
+      <ul>{ingredientsList}</ul>
     </main>
   );
 };
